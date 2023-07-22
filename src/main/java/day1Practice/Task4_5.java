6c2fb5973d2f8a3b275239404af2d02914a7634f
Aliaksei <kulevetsav@gmail.com>
2023-07-22 15:03:36 +0200

    )))

--- src/main/java/day1Practice/Task4_5.java
+++ src/main/java/day1Practice/Task4_5.java
@@ -2,6 +2,10 @@ package day1Practice;
 
 public class Task4_5 {
     public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("New day");
 
+
+        System.out.println( " hello");
     }
 }
