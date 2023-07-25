1b1caf13691d3c44f26f7c6111ac553c270c8915
Aliaksei <kulevetsav@gmail.com>
2023-07-25 14:58:34 +0200

    )))

--- src/main/java/day3Practice/arraysPrint2to5/Main.java
+++ src/main/java/day3Practice/arraysPrint2to5/Main.java
@@ -1,3 +1,12 @@
+
+/*Write a Java program that creates an array of integers and prints all the elements of that array to the screen.
+
+        Task 2 (Medium):
+        Add to the program from the previous task the calculation and output of the sum of all elements of the array.*/
+
+
+
+
 package day3Practice.arraysPrint2to5;
 
 import day3Practice.arraysPrint2to5.PrivatArrays;
