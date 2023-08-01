4c9614969ba869ddb5502ab6aa998933f6cb271c
Aliaksei <kulevetsav@gmail.com>
2023-08-01 11:14:20 +0200

    .

--- untitled/src/main/java/lottery/Lottery.java
+++ untitled/src/main/java/lottery/Lottery.java
@@ -1,7 +1,27 @@
 package lottery;
 
+import java.util.Scanner;
+
 public class Lottery {
+
+    private final int[] winningNumbers = new int[5];
+    private  int[] customerNumbers;
+
     public  void run() {
 
+    }
+    private void makeCustomerNumbers() {
+        int[] customerNumbers = new int[5];
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое значение: ");
+        customerNumbers[0] = scanner.nextInt();
+        System.out.print("Введите второе значение: ");
+        customerNumbers[1] = scanner.nextInt();
+        System.out.print("Введите третье значение: ");
+        customerNumbers[2] = scanner.nextInt();
+        System.out.print("Введите четвертое значение: ");
+        customerNumbers[3] = scanner.nextInt();
+        System.out.print("Введите пятое значение: ");
+        customerNumbers[4] = scanner.nextInt();
     }
 }
