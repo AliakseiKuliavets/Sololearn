e22c832e3195b6e5265bd89aa94cdd5ab8dbb5d7
Aliaksei <kulevetsav@gmail.com>
2023-08-01 10:18:25 +0200

    .

--- src/main/java/day5Practice/example2/Main.java
+++ src/main/java/day5Practice/example2/Main.java
@@ -1,17 +1,17 @@
 package day5Practice.example2;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers =new int[8];
-        Random random=new Random();
-        for (int i = 0; i<numbers.length;i++) {
 
-            numbers[i] = random.nextInt(1,50);
 
-            System.out.println(numbers[i]);
+        int[] arrays = {12,13,43,656,89,36,99,46,9876,4,5,7,7};
+        for (int i = 1; i < arrays.length; i = i + 2 ) {
+            arrays[i] = 0;
         }
-        }
+        System.out.println(Arrays.toString(arrays));
     }
+}
 
