b3de1880e109d0e6408ce0436c163c4cb68e4e60
Aliaksei <kulevetsav@gmail.com>
2023-08-01 11:55:55 +0200

    .

--- untitled/src/main/java/lottery/Lottery.java
+++ untitled/src/main/java/lottery/Lottery.java
@@ -1,5 +1,7 @@
 package lottery;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Lottery {
@@ -7,21 +9,35 @@ public class Lottery {
     private final int[] winningNumbers = new int[5];
     private  int[] customerNumbers;
 
-    public  void run() {
+    public void run() {
+        makeCustomerNumbers();
+        makeWinningNumbers();
+        printWinnAndCustomNumbers();
 
     }
     private void makeCustomerNumbers() {
         int[] customerNumbers = new int[5];
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите первое значение: ");
-        customerNumbers[0] = scanner.nextInt();
-        System.out.print("Введите второе значение: ");
-        customerNumbers[1] = scanner.nextInt();
-        System.out.print("Введите третье значение: ");
-        customerNumbers[2] = scanner.nextInt();
-        System.out.print("Введите четвертое значение: ");
-        customerNumbers[3] = scanner.nextInt();
-        System.out.print("Введите пятое значение: ");
-        customerNumbers[4] = scanner.nextInt();
+        System.out.println("Enter your \"Lucky numbers\"");
+        for (int i = 0; i < customerNumbers.length; i++) {
+            System.out.print((i + 1) + " number: ");
+            customerNumbers[i] = scanner.nextInt();
+            if (customerNumbers[i] < 1 || customerNumbers[i] > 50) {
+                System.out.println("Wrong numbers");
+            }
+        }
+        this.customerNumbers = customerNumbers;
+    }
+
+    private void makeWinningNumbers() {
+        Random random = new Random();
+        for (int i = 0; i < winningNumbers.length; i++) {
+            winningNumbers[i] = random.nextInt(1,51);
+        }
+    }
+
+    private void printWinnAndCustomNumbers() {
+        System.out.println("Winning numbers: " + Arrays.toString(winningNumbers));
+        System.out.println("Customer numbers: " + Arrays.toString(customerNumbers));
     }
 }
