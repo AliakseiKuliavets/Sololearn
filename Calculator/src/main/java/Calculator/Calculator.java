9fc6bdd4d3e00cc3dce53ccfc11a09f8b3832965
Aliaksei <kulevetsav@gmail.com>
2023-08-03 12:05:34 +0200

    calculator

--- Calculator/src/main/java/Calculator/Calculator.java
+++ Calculator/src/main/java/Calculator/Calculator.java
@@ -3,6 +3,7 @@ package Calculator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Calculator {
@@ -10,16 +11,28 @@ public class Calculator {
     private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public void run() {
-        String nextLine;
+        char mathChar;
         Double result;
+        System.out.print("Enter character: ");
+        mathChar = getNextLine().charAt(0);
         while (true) {
-            nextLine = getNextLine();
-            if (nextLine.equals("E")) {
+            if (mathChar == 'E') {
                 closeProgram();
             }
-            result = userInputRouter(nextLine.charAt(0));
+            result = userInputRouter(mathChar, getInputNumbersArray());
             System.out.println(result);
         }
+    }
+
+    private double[] getInputNumbersArray() {
+        System.out.print("Enter number of arguments 2 and more: ");
+        int amount = Integer.parseInt(getNextLine());
+        double[] inputNumbersArray = new double[amount];
+        for (int i = 0; i < inputNumbersArray.length; i++) {
+            System.out.print("Enter " + (i + 1) + " value: ");
+            inputNumbersArray[i] = Double.parseDouble(getNextLine());
+        }
+        return inputNumbersArray;
     }
 
     private void closeProgram() {
@@ -27,7 +40,6 @@ public class Calculator {
     }
 
     private String getNextLine() {
-        System.out.print("Enter character");
         try {
             return reader.readLine();
         } catch (IOException e) {
@@ -41,45 +53,58 @@ public class Calculator {
     }
 
 
-    private double multiplication(double first, double second) {
-        System.out.println("Multiplication result: ");
-        return first * second;
-    }
-
-    private double divide(double dividend, double divisor) {
-        if (divisor != 0.0) {
-            System.out.println("Division result: ");
-            return dividend / divisor;
-        } else {
-            System.out.println("Can't divide by zero");
-            return -1;
+    private Double multiplication(double[] inputNumbers) {
+        Double result = inputNumbers[0];
+        for (int i = 1; i < inputNumbers.length; i++) {
+            result = result * inputNumbers[i];
         }
+        System.out.print("Multiplication result: ");
+        return result;
     }
 
-    private double sum(double first, double second) {
-        System.out.println("Addition result: ");
-        return first + second;
+    private Double divide(double[] inputNumbers) {
+        Double result = inputNumbers[0];
+        for (int i = 1; i < inputNumbers.length; i++) {
+            if (inputNumbers[i] != 0.0) {
+                result = result / inputNumbers[i];
+            } else {
+                System.out.print("Can't divide by zero");
+                return null;
+            }
+        }
+        System.out.print("division result: ");
+        return result;
     }
 
-    private double subtract(double reduced, double subtracted) {
-        System.out.println("Subtraction result: ");
-        return reduced - subtracted;
+    private Double sum(double[] inputNumbers) {
+        Double result = inputNumbers[0];
+        for (int i = 1; i < inputNumbers.length; i++) {
+            result = result + inputNumbers[i];
+        }
+        System.out.print("Addition result: ");
+        return result;
+    }
+
+    private Double subtract(double[] inputNumbers) {
+        Double result = inputNumbers[0];
+        for (int i = 1; i < inputNumbers.length; i++) {
+            result = result - inputNumbers[i];
+        }
+        System.out.print("Subtraction result: ");
+        return result;
     }
 
 
-    private Double userInputRouter(char inputChar) {
+    private Double userInputRouter(char inputChar, double[] inputNumbers) {
         switch (inputChar) {
             case '*':
-                double first = getDoubleFromCommandLine();
-                double second = getDoubleFromCommandLine();
-
-                return multiplication(first, second);
+                return multiplication(inputNumbers);
             case '/':
-                return divide(getDoubleFromCommandLine(), getDoubleFromCommandLine());
+                return divide(inputNumbers);
             case '+':
-                return sum(getDoubleFromCommandLine(), getDoubleFromCommandLine());
+                return sum(inputNumbers);
             case '-':
-                return subtract(getDoubleFromCommandLine(), getDoubleFromCommandLine());
+                return subtract(inputNumbers);
             default:
                 System.out.println("Not valid value");
                 return null;
--- Calculator/src/main/java/Main.java
+++ Calculator/src/main/java/Main.java
@@ -1,3 +1,37 @@
+
+/*
+
+
+Продолжаем работу над калькулятором.
+        Обновите "Калькулятор" до версии 2,
+        Обновите функционал до производящего вычисления (+, -, *, /) с произвольным количеством чисел c
+        дробной частью.
+        Количество аргументов пользователь вводит с клавиатуры. Оно должно быть от 2 и более.
+        Тип данных вводимых аргументов должен быть Double.
+        Далее пользователь последовательно вводит значение каждого из аргументов, а затем - знак математической
+        операции, которую необходимо выполнить со значениями введенных аргументов.
+        Результат работы программы необходимо вывести в виде вычисляемого выражения и полученного результата
+        с точностью до двух чисел после запятой.]
+
+
+
+
+
+        We continue to work on the calculator.
+        Update "Calculator" to version 2,
+        Update the functional to a generating calculation (+, -, *, /) with an arbitrary number of numbers c
+        fractional part.
+        The number of arguments the user enters from the keyboard. It must be 2 or more.
+        The data type of the input arguments must be Double.
+        Next, the user sequentially enters the value of each of the arguments, and then - the sign of the mathematical
+        the operation to be performed on the values ​​of the supplied arguments.
+        The result of the program must be displayed in the form of a calculated expression and the result
+        up to two decimal places.]
+
+*/
+
+
+
 import Calculator.Calculator;
 
 public class Main {
