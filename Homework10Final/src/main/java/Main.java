38d27aaf502a4803c368ef5daa284ace252d0c08
Aliaksei <kulevetsav@gmail.com>
2023-08-03 12:58:08 +0200

    Homework

--- Homework10Final/src/main/java/Main.java
+++ Homework10Final/src/main/java/Main.java
@@ -1,3 +1,32 @@
+/*
+№1.
+
+        Увеличьте одновременное количество игроков до трех.
+        Три игрока вводят номера для игры.
+        Лотерея играет один раз.
+
+
+        №2.
+
+        **Используя библиотеку JUnit, напишите тесты, которые проверяют методы, реализованные в приложении
+        - лотерея и Utils классе.
+
+        No. 1.
+
+        Increase the simultaneous number of players to three.
+        Three players enter numbers to play.
+        The lottery is played once.
+
+
+        No. 2.
+
+        **Using the JUnit library, write tests that test the methods implemented in the application
+        - Lottery and Utils class.
+
+*/
+
+
+
 import lottery.Lottery;
 
 public class Main {
