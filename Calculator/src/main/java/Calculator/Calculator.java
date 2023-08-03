5ec32ed9a203b6701fe5567531d90ee0d366a15f
Aliaksei <kulevetsav@gmail.com>
2023-08-03 10:07:10 +0200

    calculator

--- Calculator/src/main/java/Calculator/Calculator.java
+++ Calculator/src/main/java/Calculator/Calculator.java
@@ -1,66 +1,88 @@
 package Calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Calculator {
     private final Scanner scanner = new Scanner(System.in);
+    private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public void run() {
-//while (true)
-        System.out.println(userInputRouter(getCharFromCommandLine()));
+        String nextLine;
+        Double result;
+        while (true) {
+            nextLine = getNextLine();
+            if (nextLine.equals("E")) {
+                closeProgram();
+            }
+            result = userInputRouter(nextLine.charAt(0));
+            System.out.println(result);
+        }
     }
 
-    private char getCharFromCommandLine() {
-        System.out.println("Введите символ");
-        return scanner.nextLine().charAt(0); //здесь возвращаем только первый символ
+    private void closeProgram() {
+        System.exit(1);
+    }
+
+    private String getNextLine() {
+        System.out.print("Enter character");
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private double getDoubleFromCommandLine() {
-        System.out.print("Введите число: ");
+        System.out.print("Enter the number ");
         return scanner.nextDouble();
     }
 
 
     private double multiplication(double first, double second) {
-        System.out.println("Результат умножения");
+        System.out.println("Multiplication result: ");
         return first * second;
     }
 
     private double divide(double dividend, double divisor) {
         if (divisor != 0.0) {
-            System.out.println("Результат деления: ");
-            return dividend/divisor;
+            System.out.println("Division result: ");
+            return dividend / divisor;
         } else {
-            System.out.println("На ноль делить нельзя");
+            System.out.println("Can't divide by zero");
             return -1;
         }
     }
-    private double sum ( double first, double second){
-        System.out.println("Результат сложения");
+
+    private double sum(double first, double second) {
+        System.out.println("Addition result: ");
         return first + second;
     }
 
-    private double subtract ( double reduced, double subtracted){
-        System.out.println("Результат вычитания");
+    private double subtract(double reduced, double subtracted) {
+        System.out.println("Subtraction result: ");
         return reduced - subtracted;
     }
 
 
-    private double userInputRouter ( char inputChar){
+    private Double userInputRouter(char inputChar) {
         switch (inputChar) {
             case '*':
-                return multiplication(getDoubleFromCommandLine(), getDoubleFromCommandLine());
+                double first = getDoubleFromCommandLine();
+                double second = getDoubleFromCommandLine();
+
+                return multiplication(first, second);
             case '/':
                 return divide(getDoubleFromCommandLine(), getDoubleFromCommandLine());
             case '+':
                 return sum(getDoubleFromCommandLine(), getDoubleFromCommandLine());
             case '-':
                 return subtract(getDoubleFromCommandLine(), getDoubleFromCommandLine());
-            case 'E'://exit выход из проги
-                System.exit(1); //завершает прогу, если другое значение ввели
             default:
-                System.out.println("Невалидное значение");
-                return -1;
+                System.out.println("Not valid value");
+                return null;
         }
     }
 }
